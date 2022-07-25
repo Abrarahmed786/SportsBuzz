2b14722f1c52c5cca3ec3505458eec0cff333490
Abrarahmed786 <abrarahmed417@hotmail.com>
2022-07-25 21:19:58 +0530

    fixes

---
+++ lib/ui/icc/widget/list_odi.dart
@@ -0,0 +1,312 @@
+import 'package:flutter/material.dart';
+
+class ListOdis extends StatelessWidget {
+  const ListOdis({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: <Widget>[
+        ListTile(
+          leading: Text(
+            'Rank',
+            style: TextStyle(fontSize: 16),
+          ),
+          title: Row(
+            children: [
+              Text(
+                'Team',
+                style: TextStyle(fontSize: 16),
+              ),
+              Spacer(),
+              Text(
+                'Points',
+                style: TextStyle(fontSize: 16),
+              ),
+            ],
+          ),
+          trailing: Text(
+            'Ratings',
+            style: TextStyle(fontSize: 16),
+          ),
+        ),
+        ListTile(
+          leading: Text(
+            '1',
+            style: TextStyle(fontSize: 16),
+          ),
+          title: Row(
+            children: [
+              Text(
+                'New Zealand',
+                style: TextStyle(fontSize: 16),
+              ),
+              Spacer(),
+              Text(
+                '1913',
+                style: TextStyle(fontSize: 16),
+              ),
+            ],
+          ),
+          trailing: Text(
+            '128',
+            style: TextStyle(fontSize: 16),
+          ),
+        ),
+        ListTile(
+          leading: Text(
+            '2',
+            style: TextStyle(fontSize: 16),
+          ),
+          title: Row(
+            children: [
+              Text(
+                'New Zealand',
+                style: TextStyle(fontSize: 16),
+              ),
+              Spacer(),
+              Text(
+                '1913',
+                style: TextStyle(fontSize: 16),
+              ),
+            ],
+          ),
+          trailing: Text(
+            '128',
+            style: TextStyle(fontSize: 16),
+          ),
+        ),
+        ListTile(
+          leading: Text(
+            '3',
+            style: TextStyle(fontSize: 16),
+          ),
+          title: Row(
+            children: [
+              Text(
+                'New Zealand',
+                style: TextStyle(fontSize: 16),
+              ),
+              Spacer(),
+              Text(
+                '1913',
+                style: TextStyle(fontSize: 16),
+              ),
+            ],
+          ),
+          trailing: Text(
+            '128',
+            style: TextStyle(fontSize: 16),
+          ),
+        ),
+        ListTile(
+          leading: Text(
+            '4',
+            style: TextStyle(fontSize: 16),
+          ),
+          title: Row(
+            children: [
+              Text(
+                'New Zealand',
+                style: TextStyle(fontSize: 16),
+              ),
+              Spacer(),
+              Text(
+                '1913',
+                style: TextStyle(fontSize: 16),
+              ),
+            ],
+          ),
+          trailing: Text(
+            '128',
+            style: TextStyle(fontSize: 16),
+          ),
+        ),
+        ListTile(
+          leading: Text(
+            '5',
+            style: TextStyle(fontSize: 16),
+          ),
+          title: Row(
+            children: [
+              Text(
+                'New Zealand',
+                style: TextStyle(fontSize: 16),
+              ),
+              Spacer(),
+              Text(
+                '1913',
+                style: TextStyle(fontSize: 16),
+              ),
+            ],
+          ),
+          trailing: Text(
+            '128',
+            style: TextStyle(fontSize: 16),
+          ),
+        ),
+        ListTile(
+          leading: Text(
+            '6',
+            style: TextStyle(fontSize: 16),
+          ),
+          title: Row(
+            children: [
+              Text(
+                'New Zealand',
+                style: TextStyle(fontSize: 16),
+              ),
+              Spacer(),
+              Text(
+                '1913',
+                style: TextStyle(fontSize: 16),
+              ),
+            ],
+          ),
+          trailing: Text(
+            '128',
+            style: TextStyle(fontSize: 16),
+          ),
+        ),
+        ListTile(
+          leading: Text(
+            '7',
+            style: TextStyle(fontSize: 16),
+          ),
+          title: Row(
+            children: [
+              Text(
+                'New Zealand',
+                style: TextStyle(fontSize: 16),
+              ),
+              Spacer(),
+              Text(
+                '1913',
+                style: TextStyle(fontSize: 16),
+              ),
+            ],
+          ),
+          trailing: Text(
+            '128',
+            style: TextStyle(fontSize: 16),
+          ),
+        ),
+        ListTile(
+          leading: Text(
+            '8',
+            style: TextStyle(fontSize: 16),
+          ),
+          title: Row(
+            children: [
+              Text(
+                'New Zealand',
+                style: TextStyle(fontSize: 16),
+              ),
+              Spacer(),
+              Text(
+                '1913',
+                style: TextStyle(fontSize: 16),
+              ),
+            ],
+          ),
+          trailing: Text(
+            '128',
+            style: TextStyle(fontSize: 16),
+          ),
+        ),
+        ListTile(
+          leading: Text(
+            '9',
+            style: TextStyle(fontSize: 16),
+          ),
+          title: Row(
+            children: [
+              Text(
+                'New Zealand',
+                style: TextStyle(fontSize: 16),
+              ),
+              Spacer(),
+              Text(
+                '1913',
+                style: TextStyle(fontSize: 16),
+              ),
+            ],
+          ),
+          trailing: Text(
+            '128',
+            style: TextStyle(fontSize: 16),
+          ),
+        ),
+        ListTile(
+          leading: Text(
+            '10',
+            style: TextStyle(fontSize: 16),
+          ),
+          title: Row(
+            children: [
+              Text(
+                'New Zealand',
+                style: TextStyle(fontSize: 16),
+              ),
+              Spacer(),
+              Text(
+                '1913',
+                style: TextStyle(fontSize: 16),
+              ),
+            ],
+          ),
+          trailing: Text(
+            '128',
+            style: TextStyle(fontSize: 16),
+          ),
+        ),
+        ListTile(
+          leading: Text(
+            '11',
+            style: TextStyle(fontSize: 16),
+          ),
+          title: Row(
+            children: [
+              Text(
+                'New Zealand',
+                style: TextStyle(fontSize: 16),
+              ),
+              Spacer(),
+              Text(
+                '1913',
+                style: TextStyle(fontSize: 16),
+              ),
+            ],
+          ),
+          trailing: Text(
+            '128',
+            style: TextStyle(fontSize: 16),
+          ),
+        ),
+        ListTile(
+          leading: Text(
+            '12',
+            style: TextStyle(fontSize: 16),
+          ),
+          title: Row(
+            children: [
+              Text(
+                'New Zealand',
+                style: TextStyle(fontSize: 16),
+              ),
+              Spacer(),
+              Text(
+                '1913',
+                style: TextStyle(fontSize: 16),
+              ),
+            ],
+          ),
+          trailing: Text(
+            '128',
+            style: TextStyle(fontSize: 16),
+          ),
+        ),
+      ],
+    );
+  }
+}
